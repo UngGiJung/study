bfd92efc2022dd9bfa02fc445648fd8d40b3d7c3
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-26 18:02:40 +0900

    04.26

--- study/src/p59/OperatorExample.java
+++ study/src/p59/OperatorExample.java
@@ -1,6 +1,6 @@
 package p59;
 
-public class p59 {
+public class OperatorExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- study/src/p61/OperatorExample.java
+++ study/src/p61/OperatorExample.java
@@ -1,6 +1,6 @@
 package p61;
 
-public class p61 {
+public class OperatorExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

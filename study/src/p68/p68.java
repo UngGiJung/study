d10a9da8f38bc7912342fac8cef833a14cf71eda
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-27 14:51:22 +0900

    04.27

---
+++ study/src/p68/p68.java
@@ -0,0 +1,19 @@
+package p68;
+
+public class p68 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean bPlay = true;
+		
+		System.out.println(bPlay);
+		
+		bPlay = !bPlay;
+		System.out.println(bPlay);
+		
+		bPlay = !bPlay;
+		System.out.println(bPlay);
+
+	}
+
+}

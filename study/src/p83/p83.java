b8b0af9c642210751b03a13e888c1b379479400d
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-28 13:31:06 +0900

    04.28

---
+++ study/src/p83/p83.java
@@ -0,0 +1,28 @@
+package p83;
+
+public class p83 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "java";
+		String str2 = "JDK";
+		
+		String str3 = str1 + str2;
+		String str4 = str1 + " " + str2;
+		
+		String str5 = "JDK" + 3 + 3.0;
+		            // String + int + double, 연산 우선순위 -> 따라서, 스트링+인트 = 스트링 >> 스트링+더블 = 스트링 >> JDK33.0
+		String str6 = 3 + 3.0 + "JDK";
+		            // int + double + String, 연산 우선순위 -> 따라서, 인트+더블 = 더블 >> 더블+스트링 = 스트링 >> 6.0JDK
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+		System.out.println(str5);
+		System.out.println(str6);
+		
+		
+	
+	}
+
+}
---
+++ study/src/p94/p94.java
@@ -0,0 +1,17 @@
+package p94;
+
+public class p94 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("45 & 25 = " + (45 & 25));
+		System.out.println("45 | 25 = " + (45 | 25));
+		System.out.println("45 ^ 25 = " + (45 ^ 25));
+		System.out.println("~45     = " + (~45));
+		
+		System.out.println("45 >> 25 = " + (45 >> 25));
+		System.out.println("45 << 25 = " + (45 << 25));
+//		System.out.println("45 <<< 25 = " + (45 <<< 25));
+	}
+
+}

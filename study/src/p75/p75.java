14484d1c567c7b334c1e045cd5d6744a1c835541
Class01 <Class01@DESKTOP-UR65MFL>
2023-04-28 10:27:42 +0900

    04.28

--- study/src/p75/p75.java
+++ study/src/p75/p75.java
@@ -6,7 +6,8 @@ public class p75 {
 		// TODO Auto-generated method stub
 		char c1 = 'A' + 1;
 		char c2 = 'A';
-		char c3 = (char)(c2+1); //산출타입 : int, java에서 모든 일반적 숫자는 는 정수이면서 int 타입으로 인식되고
+		char c3 = (char)(c2+1); 
+		//산출타입 : int, java에서 모든 일반적 숫자는 는 정수이면서 int 타입으로 인식되고
 		//소숫점이 있는 숫자는 실수 이면서 double 타입으로 인식됨
 //		char c4 = c2+1;
 		
@@ -15,8 +16,8 @@ public class p75 {
 		System.out.println("c1 : "+c2);
 		System.out.println("c1 : "+c3);
 		
-		System.out.println("%d\n", (int)c2);
-		System.out.println("%c\n", c2);
+		System.out.printf("%d ", (int)c2);
+		System.out.printf("%c", c2);
 		//%d = 결과값을 정수로 표현할때 씀
 		//%c = 결과값을 문자로 표현할때 씀
 		//%f = 결과값을 실수로 표현할때 씀
